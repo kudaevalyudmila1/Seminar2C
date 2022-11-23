53ffe7a923512ed214ade25a334a78fc1e1b12c2
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-23 15:09:27 +0300

    Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,18 +1,21 @@
 ﻿// Программа принимает на вход число А.
 // Найти сумму чисел от 1 до А.
 
-Console.WriteLine("введите натуральное число:");
+Console.WriteLine("Введите натуральное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int SumNumbers(int num)
 {
     int sum = 0;
-    for (int i = 0; i <= num; i++ )
+    for (int i = 0; i <= num; i++)
     {
         sum = sum + i;
     }
     return sum;
-}  
-
-int sumNumbers = SumNumbers(number);
-Console.WriteLine($"сумма чисел от 1 до {number} = {sumNumbers}");
+}
+if (number > 0)
+{
+    int sumNumbers = SumNumbers(number);
+    Console.WriteLine($"сумма чисел от 1 до {number} = {sumNumbers}");
+}
+else Console.WriteLine("Требуется ввести натуральное число:");
