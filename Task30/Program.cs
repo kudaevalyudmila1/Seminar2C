da0562a33e3bb8eafa6fcdad022edfc71928e72f
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-23 16:59:48 +0300

    Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать массив из 8 элементов, заполненных 
+//нулями и единицами вывести их на экран .
+
+int[] array = new int[8];
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]");
+}
+FillArray(array);
+PrintArray(array);
