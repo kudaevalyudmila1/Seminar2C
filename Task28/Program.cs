0dabaf528bc78d91ce75d4d5aa2cf7733996683f
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-23 16:08:14 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите  натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ProductNumbers(int num)
+{
+    int prod = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
+if (number > 0)
+{
+    int productNumbers = ProductNumbers(number);
+    Console.WriteLine($"{number} -> {productNumbers}");
+}
+else Console.WriteLine("Требуется ввести натуральное число:");
