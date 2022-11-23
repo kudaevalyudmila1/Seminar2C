f7414efb17d0a0a09b308dad51b040e6841c0b5f
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-23 15:01:02 +0300

    Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход число А.
+// Найти сумму чисел от 1 до А.
+
+Console.WriteLine("введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++ )
+    {
+        sum = sum + i;
+    }
+    return sum;
+}  
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"сумма чисел от 1 до {number} = {sumNumbers}");
