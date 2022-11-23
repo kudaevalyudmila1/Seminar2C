f9c1a5d229863426d94a5281600497894882cf5c
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-23 15:46:25 +0300

    Task26

---
+++ Task 26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите любое целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberDigit(int num)
+{
+    int n = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        n++;
+    }
+    return n == 0 ? 1 : n;
+}
+int numberDigit = NumberDigit(number);
+Console.WriteLine($"Количество  цифр в числе {number} равно  {numberDigit}");
+
+
+
