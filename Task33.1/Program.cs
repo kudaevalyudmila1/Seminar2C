292faa126a07872632ffc7c2a5e70f17efaff69d
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-26 20:44:23 +0300

    Task33.1

---
+++ Task33.1/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+bool FindNumber(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true;
+    }
+    return false;
+}
+
+int[] arr = CreateArrayRndInt(8, -8, 8);
+
+bool result = FindNumber(arr, 5);
+PrintArray(arr);
+Console.Write(result ? "-> Да" : "-> Нет");
