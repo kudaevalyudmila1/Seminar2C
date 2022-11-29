0f55157a2db21e3c00ef777db3590f846e3271dc
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-29 20:56:40 +0300

    Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите  первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int num1, int num2, int num3)
+{
+    return num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2;
+}
+Console.WriteLine(Triangle(number1, number2, number3)?"Да": "Нет");
