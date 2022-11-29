a75864413edf1dbbc734acabaff39279a08eb255
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-29 21:20:42 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите   число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string ConvertToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+Console.WriteLine(ConvertToBin(num));
