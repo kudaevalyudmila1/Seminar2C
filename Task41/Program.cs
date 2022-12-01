c63ac6707c8ccd586959e78bbdb37efaced2665b
Lyudmila <kudaevalyudmila@gmail.com>
2022-12-01 20:34:59 +0300

    Task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -22,8 +22,8 @@ int PositivNumbersCount(int[] array)
     int n = 0;
     for (int i = 0; i < array.Length; i++)
     {
-       if(array[i] > 0) 
-       n = n + 1;
+        if (array[i] > 0)
+            n++;
     }
     return n;
 }
