d7ff100eb58d3a3c1bd094ad1463001be158f702
Lyudmila <kudaevalyudmila@gmail.com>
2022-12-05 22:42:38 +0300

    Task51

---
+++ Task51/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SumElements(int[,]matrix)
+{
+    int sum = 0; 
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+        {
+            sum = sum + matrix[i, i];
+        }
+    return sum;
+}
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+int res = SumElements(array2D);
+Console.WriteLine(res);
