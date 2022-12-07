6edc3097c50091d33a42ae9b6380730144f06b40
Lyudmila <kudaevalyudmila@gmail.com>
2022-12-07 21:43:22 +0300

    Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] RepleseRowsToColumns(int[,] matrix)
+{
+    int [,] matr = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matr[i, j] = matrix[j, i];
+    }
+    return matr;
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine(" ");
+if (array2D.GetLength(0) == array2D.GetLength(1))
+{
+    int[,] newarray2D = RepleseRowsToColumns(array2D);
+    PrintMatrix(newarray2D);
+}
+else Console.WriteLine("Заменить строки столбцами в матрице невозможно.");
