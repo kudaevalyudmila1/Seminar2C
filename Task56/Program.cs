e3b91d3fba6389f4ceec21e636d6cfb60e3bc0fb
Lyudmila <kudaevalyudmila@gmail.com>
2022-12-08 17:58:32 +0300

    Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,78 @@
+﻿//Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку
+//  с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] MinSumElementsRows(int[,] matrix)
+{
+    int[] newarr = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        newarr[i] = sum;
+    }
+    return newarr;
+}
+int FindMinRows(int[] array)
+{
+    int min = array[0];
+    int minindex = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= min)
+        {
+            min = array[i];
+            minindex = i + 1;
+        }
+    }
+    return minindex;
+}
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine(String.Empty);
+int[] newarray = MinSumElementsRows(array2D);
+int result = FindMinRows(newarray);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов: {result}");
